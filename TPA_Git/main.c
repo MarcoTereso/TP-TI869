08812df6013da607af329cdbb14017ab6ad6e6eb
zmiguel <zemiguel.29@hotmail.com>
2023-09-20 16:35:29 +0100

    Elaboracao da funcao para o calculo da area do retangulo

--- TPA_Git/main.c
+++ TPA_Git/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf(">>>> MENU! <<<<\n");
+void calcularAreaRetangulo(){
+     float a,b;
+    printf("\nDigite o primeiro valor: ");
+    scanf("%f",&a);
+    printf("\nDigite o segundo valor: ");
+    scanf("%f",&b);
+    float area =(a*b);
+    printf("Area = %.1f\n",area);
+    
+}
+int main(){
+   calcularAreaRetangulo();
     return 0;
 }
