192fd035b0ca690e1c793cdd39516856ef5eff37
kastico <kastico.stash@gmail.com>
2023-09-20 16:29:57 +0100

    Funcao areaCirculo Implementada

--- TPA_Git/main.c
+++ TPA_Git/main.c
@@ -3,8 +3,16 @@
 
 #define PI 3.1415
 
-void areaCirculo(float r){
-    printf(" A Área do círculo é %f\n", PI*r*r);
+void areaCirculo(){
+    float r;
+    do {
+        printf("Insira o raio (deve ser maior que 0): ");
+        scanf("%f", &r);
+        if (r <= 0) {
+            printf("O raio deve ser maior que 0. Tenta outravez amigo!.\n");
+        }
+    } while (r <= 0);
+    printf(" A Área do círculo é %.2f\n", PI*r*r);
 }
 
 int main()
