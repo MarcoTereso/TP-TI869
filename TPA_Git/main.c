2d0805916cde856af62dcef25f5da27b7e2cef89
machado <nuno_carreira_machado@hotmail.com>
2023-09-20 16:10:55 +0100

    Implementação da função perimetroTriangulo

--- TPA_Git/main.c
+++ TPA_Git/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void perimetroTriangulo(float hip, float cat1, float cat2)
+{
+    printf("Perímetro do triângulo é %f",(hip+cat1+cat2));
+}
+
 int main()
 {
     printf(">>>> MENU! <<<<\n");
