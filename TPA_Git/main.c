0ff5e9ff1fe95503077cdc1a3e56cb8277d70cec
machado <nuno_carreira_machado@hotmail.com>
2023-09-20 16:36:54 +0100

    função com recolha de dados e verificações

--- TPA_Git/main.c
+++ TPA_Git/main.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void perimetroTriangulo(float hip, float cat1, float cat2)
+void perimetroTriangulo()
 {
-    printf("Perímetro do triângulo é %f",(hip+cat1+cat2));
+    int verifica;
+    float b, l1, l2;
+    printf("Insira a base: ");
+    do
+    {
+        verifica=scanf("%f", &b);
+    }while(verifica!=1 || b<=0);
+    printf("Insira o primeiro lado: ");
+    do
+    {
+        verifica=scanf("%f", &l1);
+    }while(verifica!=1);
+    printf("Insira o segundo lado: ");
+    do
+    {
+        verifica=scanf("%f", &l2);
+    }while(verifica!=1);
+    printf("Perímetro do triângulo é %f",(b+l1+l2));
 }
 
 int main()
