7b8f361af587b9c658d55d7c662803fbc65532df
dleitao <diomlgomes@gmail.com>
2023-09-20 16:43:08 +0100

    criacao da funcao area esfera

--- TPA_Git/main.c
+++ TPA_Git/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define PI 3.1415
 
-int main()
-{
-    printf(">>>> MENU! <<<<\n");
-    return 0;
+
+//DEFINE PI 3.14.15
+
+void areaEsfera(){
+
+    float raio, area;
+
+
+    printf("Digite valor do raio: ");
+    scanf("%f", &raio);
+
+    area = 4*PI*raio;
+
+    printf("\nArea da esfera : %.2f", raio);
 }
