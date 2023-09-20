59b8c9c1f5fb6e10c822c82e4429e93830cdbba2
FMENDES <kenwayconnor3@gmail.com>
2023-09-20 16:38:22 +0100

    calculo do numero ao quadrado

--- TPA_Git/main.c
+++ TPA_Git/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <stdio.h>
+
+//float calcularQuadradoNum(float num1, float num2) {
+//    float quadrado = num1 * num2;
+//    printf("O quadrado de %.2f * %.2f = %.2f\n", num1, num2, quadrado);
+//    return quadrado;
+//} função feita por %Francisco Mendes% calculo do numero ao quadrado
+//
+
 int main()
 {
     printf(">>>> MENU! <<<<\n");
+    //calcularQuadrado(quad); implementação da função no main
     return 0;
 }
