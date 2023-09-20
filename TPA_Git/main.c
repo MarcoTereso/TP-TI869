1d272c588c5f925508c958f62f1fabb1156a5fa5
bruno <bruno_carmo11@live.com.pt>
2023-09-20 16:45:16 +0100

    Calculo da Area do Quadrado

--- TPA_Git/main.c
+++ TPA_Git/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void area_quadrado()
+{
+    float valor;
+    float area;
+    int verifica;
+    do
+    {    
+        fflush(stdin);
+        printf("Digite o valor do lado: ");
+        //verificacao
+        verifica = scanf("%f",&valor);
+        system("cls");
+
+    }   while((verifica) = 1 && valor <=0 );// ou while((verifica) != 1 || valor <=0 )
+        area=valor*valor;
+    printf("A Area do quadrado de lado %.2f = %.2f",valor,area);
+}
+
 int main()
 {
     printf(">>>> MENU! <<<<\n");
+    area_quadrado();
     return 0;
 }
