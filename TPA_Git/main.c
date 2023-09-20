6e44196ea6352cb3ee14543a1dcb0e171d569600
egarrido <edjgarrido@gmail.com>
2023-09-20 16:35:38 +0100

    Funcao perímetro circulo

--- TPA_Git/main.c
+++ TPA_Git/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define PI 3.1415
+
+void perimetroCirculo()
+{
+    float raio, per;
+    printf("Digite o valor do Raio:  ");
+    scanf("%f", &raio);
+    per = 2 * PI * raio;
+    printf("O perímetro do círculo é %.2f\n", per);
+}
+
 int main()
 {
     printf(">>>> MENU! <<<<\n");
