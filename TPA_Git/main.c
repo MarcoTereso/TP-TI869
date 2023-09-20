ac7d709a230a599feba2c9e60cc04862eb2c146b
kastico <kastico.stash@gmail.com>
2023-09-20 16:17:14 +0100

    Funcao areaCirculo Implementada

--- TPA_Git/main.c
+++ TPA_Git/main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define PI 3.1415
+
+void areaCirculo(float r){
+    printf(" A Área do círculo é %f\n", PI*r*r);
+}
+
 int main()
 {
     printf(">>>> MENU! <<<<\n");
+
     return 0;
 }
