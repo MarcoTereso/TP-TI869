bca60dcc17cd9f6f6d3c71e716c59c5b77f7f3c6
claferia <cristiano.laferia.ak@gmail.com>
2023-09-20 16:17:46 +0100

    criacao de funcao areaTriangulo testado

--- TPA_Git/main.c
+++ TPA_Git/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int areaTriangulo()
+{
+    float b, a, op;
+   printf("\nInsira o valor da base:\n");
+   scanf("%f", &b);
+   printf("\nInsira o valor do altura:\n");
+   scanf ("%f",&a) ;
+   op=(b*a)/2;
+
+    return op;
+}
+
+
+
 int main()
 {
     printf(">>>> MENU! <<<<\n");
+   
     return 0;
 }
