875d3d483595befa4a459475116221f2a76b6375
Pedro Coutinho <pcoutinho@tic.local>
2023-09-20 16:45:25 +0100

    Função de Multiplicação

--- TPA_Git/main.c
+++ TPA_Git/main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void calculoMulti();
+
 int main()
 {
-    printf(">>>> MENU! <<<<\n");
+    calculoMulti();
     return 0;
+}
+
+
+void calculoMulti(){
+
+    float v1,v2,multi;
+    int dec;
+
+    do{
+
+    fflush(stdin);
+    printf("Insira o 1º valor: ");
+    dec = scanf("%f", &v1);
+    }while(dec != 1 || v1 <= 0);
+    do{
+    fflush(stdin);
+    printf("Insira o 2º valor: ");
+    dec = scanf("%f", &v2);
+
+    }while(v2 <= 0 || dec != 1);
+
+    multi = v1*v2;
+
+    printf("Resultado: %.2f",multi);
+
 }
