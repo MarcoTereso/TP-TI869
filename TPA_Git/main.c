be08e2ade6dc9f2d3f7f692d675783c9cbbf520e
fmarquescc <filipemarqcc2023@gmail.com>
2023-09-21 09:33:12 +0100

    chamar potencia no switch

--- TPA_Git/main.c
+++ TPA_Git/main.c
@@ -95,7 +95,7 @@ void potencia()
     printf("Potência: ");
     scanf("%d", &power);
     if (value == 0 && power == -1)
-        printf("Entrada inválida!");
+        printf("Entrada inválida!\n");
     else
         printf("%.6f ^ %d = %.6f", value, power, math_pow(value, power));
 }
@@ -184,7 +184,7 @@ int main()
 
                     break;
             case 12:
-
+                    potencia();
                     break;
             case 13:
                     calculoMulti();
