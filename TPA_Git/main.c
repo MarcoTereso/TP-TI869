c7f72d88e8cf81b3bcc78f5ed2c6aa4cf4992675
machado <nuno_carreira_machado@hotmail.com>
2023-09-21 09:15:37 +0100

    função e verificações completas

--- TPA_Git/main.c
+++ TPA_Git/main.c
@@ -8,18 +8,21 @@ void perimetroTriangulo()
     printf("Insira a base: ");
     do
     {
+        fflush(stdin);
         verifica=scanf("%f", &b);
     }while(verifica!=1 || b<=0);
     printf("Insira o primeiro lado: ");
     do
     {
+        fflush(stdin);
         verifica=scanf("%f", &l1);
-    }while(verifica!=1);
+    }while(verifica!=1 || l1<=0);
     printf("Insira o segundo lado: ");
     do
     {
+        fflush(stdin);
         verifica=scanf("%f", &l2);
-    }while(verifica!=1);
+    }while(verifica!=1 || l2<=0);
     printf("Perímetro do triângulo é %f",(b+l1+l2));
 }
 
